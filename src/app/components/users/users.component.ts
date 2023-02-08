a3e0f97842491ef639a97145cd04af1e4c854295
Omolara5861 <debbiegterra@gmail.com>
2023-02-08 13:57:35 +0100

    feat: :sparkles: Call the users service in the users component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from '../../services/user.service';
+import { Response } from '../../interfaces/response';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+usersArray: Response;
+
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getUsers(5).subscribe(
+      users => {
+        this.usersArray = users;
+      },
+      err => console.log(err),
+      () => console.log(`Complete Fetching users from Randomusers.me API`)
+    )
+  }
 }
