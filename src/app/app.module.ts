0ec9f399216f53b08b9bf5b35b2507866dbc8253
Omolara5861 <debbiegterra@gmail.com>
2023-02-08 12:18:38 +0100

    feat: :sparkles: Create http requests to fetch random user from rest api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {  HttpClientModule} from '@angular/common/http';
+
+// Components
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
@@ -14,7 +17,8 @@ import { UserDetailComponent } from './components/user-detail/user-detail.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  readonly baseUrl = 'https://randomuser.me/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(size = 10): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}?results=${size}`);
+  }
+
+  getUser(uuid: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}uuid=${uuid}`);
+  }
 }
