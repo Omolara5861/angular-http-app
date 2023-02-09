f44cc2258352766a84581221b143d0c55a5a95ab
Omolara5861 <debbiegterra@gmail.com>
2023-02-09 23:00:57 +0100

    docs: :bulb: Add comments in users component source code

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/interfaces/user';
 import { UserService } from '../../services/user.service';
-import {CustomResponse } from '../../interfaces/response';
+import { CustomResponse } from '../../interfaces/response';
 
 @Component({
   selector: 'app-users',
@@ -10,12 +10,15 @@ import {CustomResponse } from '../../interfaces/response';
 })
 export class UsersComponent implements OnInit {
 
+/** An array that stores the users returned by the API */
 usersArray: CustomResponse;
 
   constructor(private userService: UserService) {}
+
   ngOnInit(): void {
-    this.userService.getUsers(5).subscribe(
-      users => {
+    /**Call the getUsers method in the user service with 8 so the API would return 8 users.  If successful, the users fetched @users would be stored in the @userArray but if there is an error, it would be logged to the console*/
+    this.userService.getUsers(8).subscribe(
+      (users: CustomResponse) => {
         this.usersArray = users;
       },
       err => console.log(err),
