20ebc00db6cb56156ce3b96c46b2368f4c23cc66
Omolara5861 <debbiegterra@gmail.com>
2023-02-13 20:26:24 +0100

    feat: :sparkles: Send access allow origin to request header

--- src/app/interceptor/cors.interceptor.ts
+++ src/app/interceptor/cors.interceptor.ts
@@ -5,14 +5,20 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, retry, catchError, EMPTY } from 'rxjs';
+import { CustomResponse } from '../interfaces/response';
 
 @Injectable()
 export class CorsInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<CustomResponse>, next: HttpHandler): Observable<HttpEvent<CustomResponse>> {
+    /** Sends Access-Control-Allow-Origin to request header */
+    const reqCopy = request.clone({setHeaders: {'Access-Control-Allow-Origin': '*'}});
+    return next.handle(reqCopy).pipe(
+      /** Incase of an error this method retries the request 3 times before it eventually fails*/
+      retry(3)
+    );
   }
 }
