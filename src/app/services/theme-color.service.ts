24f55f426a9cbdfcd2833dc5a26857b949b7cbdd
Omolara5861 <debbiegterra@gmail.com>
2023-02-15 15:23:17 +0100

    feat: :sparkles: Create theme service

--- src/app/services/theme-color.service.ts
+++ src/app/services/theme-color.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeColorService {
 
-  constructor() { }
+  private colorTheme: string;
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  initTheme() {
+    this.getTheme();
+    this.renderer.addClass(document.body, this.colorTheme);
+  }
+
+  isDarkMode(): boolean {
+    return this.colorTheme === 'dark-mode';
+  }
+
+
+  private setTheme(theme: 'light-mode'| 'dark-mode'): void {
+    this.colorTheme = theme;
+    localStorage.setItem('user-theme', theme);
+  }
+
+  getTheme(): void {
+    if(localStorage.getItem('user-theme')) {
+      this.colorTheme = localStorage.getItem('user-theme')!;
+    }
+    else {
+      this.colorTheme = 'light-mode';
+    }
+  }
+
+  updateTheme(theme: 'light-mode' | 'dark-mode'): void {
+    this.setTheme(theme);
+    const previousTheme = (theme === 'light-mode' ? 'dark-mode': 'light-mode');
+    this.renderer.removeClass(document.body, previousTheme);
+    this.renderer.addClass(document.body, theme);
+  }
 }
+
