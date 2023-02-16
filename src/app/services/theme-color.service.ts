ef4edc36a09da8fe39d539cb568d623de3d729fe
Omolara5861 <debbiegterra@gmail.com>
2023-02-16 05:06:54 +0100

    docs: :bulb: Add comments to theme service

--- src/app/services/theme-color.service.ts
+++ src/app/services/theme-color.service.ts
@@ -12,21 +12,24 @@ export class ThemeColorService {
     this.renderer = rendererFactory.createRenderer(null, null);
   }
 
+    /** This method calls @getTheme() to retrieve the current theme, then adds that theme to the body element of the DOM */
   initTheme() {
     this.getTheme();
     this.renderer.addClass(document.body, this.colorTheme);
   }
 
+  /** This method returns a boolean based on whether the current theme is 'dark-mode' */
   isDarkMode(): boolean {
     return this.colorTheme === 'dark-mode';
   }
 
-
+ /** The setTheme() method sets the colorTheme variable to the passed-in theme, and stores the theme in local storage using @localStorage.setItem(). */
   private setTheme(theme: 'light-mode'| 'dark-mode'): void {
     this.colorTheme = theme;
     localStorage.setItem('user-theme', theme);
   }
 
+  /** The getTheme() method retrieves the theme from local storage using localStorage.getItem(), and sets colorTheme to the retrieved value. If no theme is found in local storage, it defaults to 'light-mode'. */
   getTheme(): void {
     if(localStorage.getItem('user-theme')) {
       this.colorTheme = localStorage.getItem('user-theme')!;
@@ -35,6 +38,8 @@ export class ThemeColorService {
       this.colorTheme = 'light-mode';
     }
   }
+
+  /** The updateTheme() method calls setTheme() to update the theme in both colorTheme and local storage, then removes the previous theme class from the body using renderer.removeClass(), and adds the new theme class using renderer.addClass(). */
 
   updateTheme(theme: 'light-mode' | 'dark-mode'): void {
     this.setTheme(theme);
